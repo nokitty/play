2dea10df71ced93ff479197401deecdc823d4a9a
nokitty <305870697@qq.com>
2015-10-22 17:27:40 +0800

    1

---
+++ reflect/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace reflect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new { id = 10, age = 20 };
+            foreach (var item in obj.GetType().GetProperties())
+            {
+                var str = string.Format("name={0},value={1}", item.Name, item.GetValue(obj,null));
+                Console.WriteLine(str);
+            }
+            Console.Read();
+        }
+    }
+}
