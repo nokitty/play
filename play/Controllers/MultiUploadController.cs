15d428b492b4c0fd25bf3e15dc68139223a4189d
nokitty <305870697@qq.com>
2015-11-07 10:56:47 +0800

    1

---
+++ play/Controllers/MultiUploadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace play.Controllers
+{
+    public class MultiUploadController : Controller
+    {
+        //
+        // GET: /MultiUpload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] file)
+        {
+            return View();
+        }
+    }
+}
