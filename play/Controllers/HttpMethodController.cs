07b592ec81abbc9901cc719bd86cd1de41f5f1e0
nokitty <305870697@qq.com>
2015-12-02 14:42:46 +0800

    1

---
+++ play/Controllers/HttpMethodController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace play.Controllers
+{
+    public class HttpMethodController : Controller
+    {
+        //Get
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        ActionResult Create()
+        {
+            return View();
+        }
+
+        ActionResult Update()
+        {
+            return View();
+        }
+
+        ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
